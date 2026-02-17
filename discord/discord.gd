f93c61179b13fafba285fb6aae8fd5e36319e5ff
Theo <git@theo.is-a.dev>
2026-02-17 19:25:49 +0300

    fix: properly load the image cache

--- discord/discord.gd
+++ discord/discord.gd
@@ -1,17 +1,22 @@
-class_name Discord
-
+# FIXME: `Discord` shouldn't be *the* global API wrapper thingy
+extends Node
 const BASE_URL: String = "https://cdn.discordapp.com"
 
-static var image_cache: Variant = preload("res://discord/image_cache.gd").new()
+const ImageCache: GDScript = preload("res://discord/image_cache.gd")
 
-static func get_avatar(user_id: String, avatar_id: String, callback: Callable) -> void:
+@onready var image_cache: ImageCache = ImageCache.new()
+
+func _ready() -> void:
+	add_child(image_cache)
+
+func get_avatar(user_id: String, avatar_id: String, callback: Callable) -> void:
 	var url: String = "%s/avatars/%s/%s.webp?size=64" % [BASE_URL, user_id, avatar_id]
-	image_cache.get_or_request(url, callback)
+	self.image_cache.get_or_request(url, callback)
 
-static var token: String:
+var token: String:
 	get:
 		return OS.get_environment("TOKEN")
 
-static var channel: String:
+var channel: String:
 	get:
 		return OS.get_environment("CHANNEL")
