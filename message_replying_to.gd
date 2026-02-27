e7d2a90153271aead27c332cc57f8895a8fae83b
loqor <loqor@amblelabs.dev>
2026-02-27 17:44:51 -0600

    Additional edits to replying.

--- message_replying_to.gd
+++ message_replying_to.gd
@@ -3,5 +3,5 @@ extends Container
 
 func set_message(message: Message) -> void:
 	$RichTextLabel.from_tokens(message.tokens, 0, 24)
-	$Author.text = message.author_name
+	$Author.text = "@" + message.author_name
 	$Rounder/Avatar.texture = await Discord.get_avatar(message.author_id, message.author_avatar)
